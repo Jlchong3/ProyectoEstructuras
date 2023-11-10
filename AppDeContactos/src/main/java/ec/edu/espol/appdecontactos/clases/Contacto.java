2aecccb8d9793a0b2ff8bc8ebbd82c166b19d331
Alex <licazago@fiec.espol.edu.ec>
2023-11-09 20:17:40 -0500

    cambios guardados

---
+++ AppDeContactos/src/main/java/ec/edu/espol/appdecontactos/clases/Contacto.java
@@ -0,0 +1,24 @@
+package ec.edu.espol.appdecontactos.clases;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contacto{
+
+    public ArrayList<Integer> telefono;
+    public String redesSociales;
+    public DoubleCircularLinkedList<String> foto;
+    public String correoPersonal;
+    public String correoTrabajo; 
+    public String correoProvisional;
+    public String fechas;
+    public Contacto contactoRelacionado;
+    public String nota;
+
+    public void registrarContacto()
+    {
+        
+        
+    }
+
+}
