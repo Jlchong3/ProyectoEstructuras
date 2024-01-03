13228a73a05c67ed41a24d506d2398da39fa4fa3
Alex <licazago@fiec.espol.edu.ec>
2024-01-03 18:44:08 -0500

    Creacion del proyecto xd

---
+++ SegundoParcial/tresEnRaya/src/main/java/ec/edu/espol/tresenraya/PrimaryController.java
@@ -0,0 +1,12 @@
+package ec.edu.espol.tresenraya;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
--- SegundoParcial/tresEnRaya/src/main/java/module-info.java
+++ SegundoParcial/tresEnRaya/src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module ec.edu.espol.tresenraya {
     requires javafx.controls;
+    requires javafx.fxml;
+
+    opens ec.edu.espol.tresenraya to javafx.fxml;
     exports ec.edu.espol.tresenraya;
 }
