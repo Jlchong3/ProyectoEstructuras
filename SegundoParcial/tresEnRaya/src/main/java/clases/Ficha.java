a1d988851bee53350ba8916a33636976c50262c2
Alex <licazago@fiec.espol.edu.ec>
2024-01-12 15:35:17 -0500

    metodos en ficha

--- SegundoParcial/tresEnRaya/src/main/java/clases/Ficha.java
+++ SegundoParcial/tresEnRaya/src/main/java/clases/Ficha.java
@@ -4,13 +4,64 @@
  */
 package clases;
 
+import Excepciones.NonValidMove;
+
 /**
  *
  * @author Migi
  */
-public class Ficha {
+public abstract class Ficha {
     public Tipo tipo;
     public int x;
     public int y;
+    protected Tablero t;
+    
+    public Ficha(Tipo tipo, int x, int y, Tablero t) {
+        this.tipo = tipo;
+        this.x = x;
+        this.y = y;
+        this.t= t;
+    }
+    
+    public abstract void validarMovimiento(int x, int y) throws NonValidMove;
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Tablero getT() {
+        return t;
+    }
+
+    public void setT(Tablero t) {
+        this.t = t;
+    }
+
+    @Override
+    public String toString() {
+        return "Ficha{" + "tipo=" + tipo + ", x=" + x + ", y=" + y + ", t=" + t + '}';
+    }
+    
+    
 
 }
