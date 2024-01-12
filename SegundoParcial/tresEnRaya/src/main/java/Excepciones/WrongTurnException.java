36df81bb716b5dffcd833371c7263e0955abf3fe
Alex <licazago@fiec.espol.edu.ec>
2024-01-12 15:09:51 -0500

    creacion de la clase ficha y unas excepciones

---
+++ SegundoParcial/tresEnRaya/src/main/java/Excepciones/WrongTurnException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package Excepciones;
+
+/**
+ *
+ * @author Migi
+ */
+public class WrongTurnException extends Exception {
+
+    public WrongTurnException(String message) {
+        super(message);
+    }
+}
